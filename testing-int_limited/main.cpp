658061036f6e140182e11ba2e8091b126407b942
DuneRanger <daniel.culliver@gmail.com>
2025-01-30 00:15:54 +0100

    int_limited basics for testing

--- testing-int_limited/main.cpp
+++ testing-int_limited/main.cpp
@@ -9,7 +9,8 @@
 #include "../int_limited.hpp"
 
 typedef boost::multiprecision::cpp_int boostInt;
-typedef customBigInt::int_limited int_limited;
+
+using namespace customBigInt;
 
 // From https://www.reddit.com/r/C_Programming/comments/ozew2u/comment/h7zijm8
 uint64_t rnd64(uint64_t n)
@@ -51,54 +52,188 @@ std::vector<boostInt> generateBoostIntegers(int count = 1000, int bitSize = 1024
 	bitLimiter <<= bitSize;
 
 	for (int i = values.size(); i < count; i++) {
-		int curNumWords = bitSize/64;
-		if (randState % 8 < 5) {
+		int curNumWords = bitSize/64 + (bitSize%64 > 0);
+		if (curNumWords > 1 && randState % 8 < 5) {
 			curNumWords -= randState%8;
+			curNumWords = std::max(curNumWords, 1);
 		}
-		if (randState % 7 < 3) {
+		if (curNumWords > 3 && randState % 7 < 3) {
 			curNumWords /= 2;
 		}
-		if (randState % 6 > 3) {
+		if (curNumWords > 5 && randState % 6 > 3) {
 			curNumWords /= 3;
 		}
 		std::vector<uint64_t> words = genMultipleUint64(randState, curNumWords);
 		std::reverse(words.begin(), words.end());
-		words[0] &= UINT64_MAX >> 1;
 
 		boostInt boostNum;
 		boost::multiprecision::import_bits(boostNum, words.begin(), words.end());
+		boostNum %= bitLimiter;
+		boostNum >>= 1;
 		if (randState%3 == 0) {
 			boostNum *= -1;
 		}
 		values.push_back(boostNum);
+		randState += curNumWords;
 	}
 	return values;
 }
 
 // Reminder: When changing the constants here, change them in generateBoostIntegers as well
-std::vector<int_limited> generateInt_limited(int count = 1000, int bitSize = 1024, uint64_t randState = 1) {
-	std::vector<int_limited> values = {0, 1, -1, 2, -2, 17, -17};
+template <int bitSize>
+std::vector<int_limited<bitSize>> generateInt_limited(int count = 1000, uint64_t randState = 1) {
+	std::vector<int_limited<bitSize>> values = {0, 1, -1, 2, -2, 17, -17};
 
 	for (int i = values.size(); i < count; i++) {
-		int curNumWords = bitSize/64;
-		if (randState % 8 < 5) {
+		int curNumWords = bitSize/64 + (bitSize%64 > 0);
+		if (curNumWords > 1 && randState % 8 < 5) {
 			curNumWords -= randState%8;
+			curNumWords = std::max(curNumWords, 1);
 		}
-		if (randState % 7 < 3) {
+		if (curNumWords > 3 && randState % 7 < 3) {
 			curNumWords /= 2;
 		}
-		if (randState % 6 > 3) {
+		if (curNumWords > 5 && randState % 6 > 3) {
 			curNumWords /= 3;
 		}
 		std::vector<uint64_t> words = genMultipleUint64(randState, curNumWords);
-		words[curNumWords-1] &= UINT64_MAX >> 1;
 
-		int_limited<bitSize> curValue;
-		
+		int_limited<bitSize> curValue = 0;
+		curValue.importBits(words);
+		curValue >>= 1;
 		if (randState%3 == 0) {
 			curValue = ~curValue + 1;
 		}
 		values.push_back(curValue);
+		randState += curNumWords;
 	}
 	return values;
+}
+
+template <int bitSize>
+bool int_limitedEqualBoost(int_limited<bitSize> a, boostInt b) {
+	// std::cout << std::bitset<64>((uint64_t)a) << " " << std::bitset<64>((int64_t)b) << std::endl;
+	if (a < 0) {
+		a = ~a + 1;
+		b *= -1;
+	}
+	// std::cout << std::bitset<64>((uint64_t)a) << " " << std::bitset<64>((int64_t)b) << std::endl;
+	int rem = bitSize%64;
+	int wordCount = bitSize / 64 + (rem > 0);
+	for (int i = 0; i < wordCount; i++) {
+		uint64_t boostTrunc = (uint64_t)(b & UINT64_MAX);
+		if (i == wordCount -1 && rem > 0) {
+			boostTrunc &= UINT64_MAX >> (64 - rem);
+		}
+		if ((uint64_t)a != boostTrunc) return false;
+		a >>= 64;
+		b >>= 64;
+	}
+	return true;
+}
+
+template <int bitSize>
+bool verifyCorrectnessOfInt_limited(int testNumberCount = 1000, uint64_t randState = 1) {
+	std::cout << "====================================================================================================" << std::endl;
+	std::cout << "VERIFYING ARITHMETIC CORRECTNESS OF " << int_limited<bitSize>::className() << " WITH BOOST cpp_int" << std::endl;
+	std::cout << "====================================================================================================" << std::endl;
+
+	std::cout << "GENERATING TEST NUMBERS..." << std::endl;
+
+    std::vector<boostInt> testNumbersBoost = generateBoostIntegers(testNumberCount, bitSize, randState);
+	boostInt bitLimiter = 1;
+	bitLimiter <<= bitSize;
+
+	std::vector<int_limited<bitSize>> testNumbersInt_limited = generateInt_limited<bitSize>(testNumberCount, randState);
+
+	// for (int i = 0; i < 50; i++) {
+	// 	std::cout << i << " boostInt : " << (int64_t)testNumbersBoost[i] << std::endl;
+	// 	std::cout << i << " int_limi : " << (int)testNumbersInt_limited[i] << std::endl;
+	// 	std::cout << int_limitedEqualBoost(testNumbersInt_limited[i], testNumbersBoost[i]) << std::endl;
+	// }
+	
+	std::cout << "------------------" << std::endl;
+	std::cout << "VERIFYING ADDITION" << std::endl;
+	for (int i = 0; i < testNumberCount; i++) {
+		for (int j = 0; j < testNumberCount; j++) {
+			boostInt boostResult = testNumbersBoost[i] + testNumbersBoost[j];
+			boostResult %= bitLimiter;
+			int_limited<bitSize> myResult = testNumbersInt_limited[i] + testNumbersInt_limited[j];
+			if (!int_limitedEqualBoost<bitSize>(myResult, boostResult)) {
+				std::cout << "FAILED: " << testNumbersBoost[i] << " + " << testNumbersBoost[j] << std::endl;
+				std::cout << boostResult << " " << (uint64_t)(myResult >> 64) << " " << (uint64_t)myResult << std::endl;
+				return false;
+			}
+		}
+	}
+	std::cout << "PASSED ADDITION" << std::endl;
+	std::cout << "------------------" << std::endl;
+
+	std::cout << "VERIFYING SUBTRACTION" << std::endl;
+	for (int i = 0; i < testNumberCount; i++) {
+		for (int j = 0; j < testNumberCount; j++) {
+			boostInt boostResult = testNumbersBoost[i] - testNumbersBoost[j];
+			int_limited<bitSize> myResult = testNumbersInt_limited[i] - testNumbersInt_limited[j];
+			if (!int_limitedEqualBoost<bitSize>(myResult, boostResult)) {
+				std::cout << "FAILED: " << testNumbersBoost[i] << " - " << testNumbersBoost[j] << std::endl;
+				return false;
+			}
+		}
+	}
+	std::cout << "PASSED SUBTRACTION" << std::endl;
+	std::cout << "------------------" << std::endl;
+
+	std::cout << "VERIFYING MULTIPLICATION" << std::endl;
+	for (int i = 0; i < testNumberCount; i++) {
+		for (int j = 0; j < testNumberCount; j++) {
+			boostInt boostResult = testNumbersBoost[i] * testNumbersBoost[j];
+			int_limited<bitSize> myResult = testNumbersInt_limited[i] * testNumbersInt_limited[j];
+			if (!int_limitedEqualBoost<bitSize>(myResult, boostResult)) {
+				std::cout << "FAILED: " << testNumbersBoost[i] << " * " << testNumbersBoost[j] << std::endl;
+				return false;
+			}
+		}
+	}
+	std::cout << "PASSED MULTIPLICATION" << std::endl;
+	std::cout << "------------------" << std::endl;
+	
+	std::cout << "VERIFYING DIVISION" << std::endl;
+	for (int i = 0; i < testNumberCount; i++) {
+		for (int j = 0; j < testNumberCount; j++) {
+			if (testNumbersBoost[j] == 0) continue;
+			boostInt boostResult = testNumbersBoost[i] / testNumbersBoost[j];
+			int_limited<bitSize> myResult = testNumbersInt_limited[i] / testNumbersInt_limited[j];
+			if (!int_limitedEqualBoost<bitSize>(myResult, boostResult)) {
+				std::cout << "FAILED: " << testNumbersBoost[i] << " / " << testNumbersBoost[j] << std::endl;
+				return false;
+			}
+		}
+	}
+	std::cout << "PASSED DIVISION" << std::endl;
+	std::cout << "------------------" << std::endl;
+
+	std::cout << "VERIFYING MODULO" << std::endl;
+	for (int i = 0; i < testNumberCount; i++) {
+		for (int j = 0; j < testNumberCount; j++) {
+			if (testNumbersBoost[j] == 0) continue;
+			boostInt boostResult = testNumbersBoost[i] % testNumbersBoost[j];
+			int_limited<bitSize> myResult = testNumbersInt_limited[i] % testNumbersInt_limited[j];
+			if (!int_limitedEqualBoost<bitSize>(myResult, boostResult)) {
+				std::cout << "FAILED: " << testNumbersBoost[i] << " % " << testNumbersBoost[j] << std::endl;
+				return false;
+			}
+		}
+	}
+	std::cout << "PASSED MODULO" << std::endl;
+	std::cout << "------------------" << std::endl;
+
+	return true;
+}
+
+int main() {
+	int testCaseAmount = 100;
+	uint64_t randState = 1;
+	verifyCorrectnessOfInt_limited<64>(testCaseAmount, randState);
+
+	return 0;
 }
