7aede8d414a2c0f13fb4711b0c66f274b8268b13
DuneRanger <daniel.culliver@gmail.com>
2025-02-01 17:10:32 +0100

    int_limited - testing prepared
    Includes change of formatting for shorter logs, option of printing test numbers and prepared testCase amounts for different bitSizes

--- testing-int_limited/main.cpp
+++ testing-int_limited/main.cpp
@@ -113,12 +113,10 @@ std::vector<int_limited<bitSize>> generateInt_limited(int count = 1000, uint64_t
 
 template <int bitSize>
 bool int_limitedEqualBoost(int_limited<bitSize> a, boostInt b) {
-	// std::cout << std::bitset<64>((uint64_t)a) << " " << std::bitset<64>((int64_t)b) << std::endl;
 	if (a < 0) {
 		a = ~a + 1;
 		b *= -1;
 	}
-	// std::cout << std::bitset<64>((uint64_t)a) << " " << std::bitset<64>((int64_t)b) << std::endl;
 	int rem = bitSize%64;
 	int wordCount = bitSize / 64 + (rem > 0);
 	for (int i = 0; i < wordCount; i++) {
@@ -133,11 +131,52 @@ bool int_limitedEqualBoost(int_limited<bitSize> a, boostInt b) {
 	return true;
 }
 
+template <typename T>
+void printIntWords(T a, bool mult = true) {
+	std::vector<uint64_t> words;
 	char sign = ' ';
+	// Because of boost integers and their casting
+	if (a < 0) {
+		sign = '-';
+		if (mult) a *= -1;
+		else a = ~a + 1;
+	}
+	do {
 		words.push_back((uint64_t)a);
 		a >>= 64;
+	} while (a != 0);
+	std::cout << sign << "1 * ";
+	for (int i = words.size()-1; i > -1; i--) {
+		std::cout << words[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+// Prints a vector of boostInt and int_limited<bitSize> alternating for easy comparison
+// Has an option to either print each 64 bit word individually, or as a whole string in base 10 (default)
 template <int bitSize>
-bool verifyCorrectnessOfInt_limited(int testNumberCount = 1000, uint64_t randState = 1) {
+void printGeneratedNumbers(std::vector<boostInt>& testNumbersBoost, std::vector<int_limited<bitSize>>& testNumbersInt_limited, bool printAsWords = false) {
+	if (printAsWords) {
+		for (int i = 0; i < testNumbersBoost.size(); i++) {
+			std::cout << i << " boostInt : ";
+			printIntWords<boostInt>(testNumbersBoost[i]);
+			std::cout << i << " int_limi : ";
+			printIntWords<int_limited<bitSize>>(testNumbersInt_limited[i], false);
+			std::cout << "Bits equal?: " << int_limitedEqualBoost(testNumbersInt_limited[i], testNumbersBoost[i]) << std::endl;
+		}
+	}
+	else {
+		for (int i = 0; i < testNumbersBoost.size(); i++) {
+			std::cout << i << " boostInt : " << testNumbersBoost[i] << std::endl;
+			std::cout << i << " int_limi : " << testNumbersInt_limited[i] << std::endl;;
+			std::cout << "Bits equal?: " << int_limitedEqualBoost(testNumbersInt_limited[i], testNumbersBoost[i]) << std::endl;
+		}
+	}
+	return;
+}
+
+template <int bitSize>
+bool verifyCorrectnessOfInt_limited(int testNumberCount = 1000, uint64_t randState = 1, bool printTestNumbers = false) {
 	std::cout << "====================================================================================================" << std::endl;
 	std::cout << "VERIFYING ARITHMETIC CORRECTNESS OF " << int_limited<bitSize>::className() << " WITH BOOST cpp_int" << std::endl;
 	std::cout << "====================================================================================================" << std::endl;
@@ -150,94 +189,128 @@ bool verifyCorrectnessOfInt_limited(int testNumberCount = 1000, uint64_t randSta
 
 	std::vector<int_limited<bitSize>> testNumbersInt_limited = generateInt_limited<bitSize>(testNumberCount, randState);
 
-	// for (int i = 0; i < 50; i++) {
-	// 	std::cout << i << " boostInt : " << (int64_t)testNumbersBoost[i] << std::endl;
-	// 	std::cout << i << " int_limi : " << (int)testNumbersInt_limited[i] << std::endl;
-	// 	std::cout << int_limitedEqualBoost(testNumbersInt_limited[i], testNumbersBoost[i]) << std::endl;
-	// }
+	if (testNumbersBoost.size() != testNumbersInt_limited.size()) {
+		std::cout << "\033[1;31mERROR: testNumbersBoost.size() and testNumbersInt_limited.size() do not equal!\033[0m" << std::endl;
+		return false;
+	}
+
+	if (printTestNumbers) printGeneratedNumbers<bitSize>(testNumbersBoost, testNumbersInt_limited, false);
 	
-	std::cout << "------------------" << std::endl;
-	std::cout << "VERIFYING ADDITION" << std::endl;
+	std::cout << "VERIFYING ADDITION: ";
 	for (int i = 0; i < testNumberCount; i++) {
 		for (int j = 0; j < testNumberCount; j++) {
 			boostInt boostResult = testNumbersBoost[i] + testNumbersBoost[j];
 			boostResult %= bitLimiter;
 			int_limited<bitSize> myResult = testNumbersInt_limited[i] + testNumbersInt_limited[j];
 			if (!int_limitedEqualBoost<bitSize>(myResult, boostResult)) {
-				std::cout << "FAILED: " << testNumbersBoost[i] << " + " << testNumbersBoost[j] << std::endl;
-				std::cout << boostResult << " " << (uint64_t)(myResult >> 64) << " " << (uint64_t)myResult << std::endl;
+				std::cout << "\033[1;31mFAILED: " << testNumbersBoost[i] << " + " << testNumbersBoost[j] << "\033[0m" << std::endl;
 				return false;
 			}
 		}
 	}
-	std::cout << "PASSED ADDITION" << std::endl;
-	std::cout << "------------------" << std::endl;
+	std::cout << "\033[32mPASSED ADDITION\033[0m" << std::endl;
 
-	std::cout << "VERIFYING SUBTRACTION" << std::endl;
+	std::cout << "VERIFYING SUBTRACTION: ";
 	for (int i = 0; i < testNumberCount; i++) {
 		for (int j = 0; j < testNumberCount; j++) {
 			boostInt boostResult = testNumbersBoost[i] - testNumbersBoost[j];
 			int_limited<bitSize> myResult = testNumbersInt_limited[i] - testNumbersInt_limited[j];
 			if (!int_limitedEqualBoost<bitSize>(myResult, boostResult)) {
-				std::cout << "FAILED: " << testNumbersBoost[i] << " - " << testNumbersBoost[j] << std::endl;
+				std::cout << "\033[1;31mFAILED: " << testNumbersBoost[i] << " - " << testNumbersBoost[j] << "\033[0m" << std::endl;
 				return false;
 			}
 		}
 	}
-	std::cout << "PASSED SUBTRACTION" << std::endl;
-	std::cout << "------------------" << std::endl;
+	std::cout << "\033[32mPASSED SUBTRACTION\033[0m" << std::endl;
 
-	std::cout << "VERIFYING MULTIPLICATION" << std::endl;
+	std::cout << "VERIFYING MULTIPLICATION: ";
 	for (int i = 0; i < testNumberCount; i++) {
 		for (int j = 0; j < testNumberCount; j++) {
 			boostInt boostResult = testNumbersBoost[i] * testNumbersBoost[j];
 			int_limited<bitSize> myResult = testNumbersInt_limited[i] * testNumbersInt_limited[j];
 			if (!int_limitedEqualBoost<bitSize>(myResult, boostResult)) {
-				std::cout << "FAILED: " << testNumbersBoost[i] << " * " << testNumbersBoost[j] << std::endl;
+				std::cout << "\033[1;31mFAILED: " << testNumbersBoost[i] << " * " << testNumbersBoost[j] << "\033[0m" << std::endl;
 				return false;
 			}
 		}
 	}
-	std::cout << "PASSED MULTIPLICATION" << std::endl;
-	std::cout << "------------------" << std::endl;
+	std::cout << "\033[32mPASSED MULTIPLICATION\033[0m" << std::endl;
 	
-	std::cout << "VERIFYING DIVISION" << std::endl;
+	std::cout << "VERIFYING DIVISION: ";
 	for (int i = 0; i < testNumberCount; i++) {
 		for (int j = 0; j < testNumberCount; j++) {
 			if (testNumbersBoost[j] == 0) continue;
 			boostInt boostResult = testNumbersBoost[i] / testNumbersBoost[j];
 			int_limited<bitSize> myResult = testNumbersInt_limited[i] / testNumbersInt_limited[j];
 			if (!int_limitedEqualBoost<bitSize>(myResult, boostResult)) {
-				std::cout << "FAILED: " << testNumbersBoost[i] << " / " << testNumbersBoost[j] << std::endl;
+				std::cout << "\033[1;31mFAILED: " << testNumbersBoost[i] << " / " << testNumbersBoost[j] << "\033[0m" << std::endl;
+				printIntWords<boostInt>(testNumbersBoost[i]);
+				printIntWords<int_limited<bitSize>>(testNumbersInt_limited[i], false);
+				printIntWords<boostInt>(boostResult);
+				printIntWords<int_limited<bitSize>>(myResult, false);
 				return false;
 			}
 		}
 	}
-	std::cout << "PASSED DIVISION" << std::endl;
-	std::cout << "------------------" << std::endl;
+	std::cout << "\033[32mPASSED DIVISION\033[0m" << std::endl;
 
-	std::cout << "VERIFYING MODULO" << std::endl;
+	std::cout << "VERIFYING MODULO: ";
 	for (int i = 0; i < testNumberCount; i++) {
 		for (int j = 0; j < testNumberCount; j++) {
 			if (testNumbersBoost[j] == 0) continue;
 			boostInt boostResult = testNumbersBoost[i] % testNumbersBoost[j];
 			int_limited<bitSize> myResult = testNumbersInt_limited[i] % testNumbersInt_limited[j];
 			if (!int_limitedEqualBoost<bitSize>(myResult, boostResult)) {
-				std::cout << "FAILED: " << testNumbersBoost[i] << " % " << testNumbersBoost[j] << std::endl;
+				std::cout << "\033[1;31mFAILED: " << testNumbersBoost[i] << " % " << testNumbersBoost[j] << "\033[0m" << std::endl;
+				printIntWords<boostInt>(testNumbersBoost[i]);
+				printIntWords<int_limited<bitSize>>(testNumbersInt_limited[i], false);
+				printIntWords<boostInt>(boostResult);
+				printIntWords<int_limited<bitSize>>(myResult, false);
 				return false;
 			}
 		}
 	}
-	std::cout << "PASSED MODULO" << std::endl;
-	std::cout << "------------------" << std::endl;
+	std::cout << "\033[32mPASSED MODULO\033[0m" << std::endl;
 
 	return true;
 }
 
 int main() {
-	int testCaseAmount = 100;
+	int testCaseAmount = 2000;
 	uint64_t randState = 1;
-	verifyCorrectnessOfInt_limited<64>(testCaseAmount, randState);
+
+
+	// Tests multiples of 64 and different offsets
+	verifyCorrectnessOfInt_limited<16>(testCaseAmount, randState);
+	verifyCorrectnessOfInt_limited<32>(testCaseAmount, randState);
+	verifyCorrectnessOfInt_limited<64*1>(testCaseAmount, randState);
+	verifyCorrectnessOfInt_limited<64*1 + 1>(testCaseAmount, randState);
+	verifyCorrectnessOfInt_limited<64*2>(testCaseAmount, randState);
+	verifyCorrectnessOfInt_limited<64*2 + 2>(testCaseAmount, randState);
+	testCaseAmount = 1000;
+	verifyCorrectnessOfInt_limited<64*3>(testCaseAmount, randState);
+	verifyCorrectnessOfInt_limited<64*3 + 3>(testCaseAmount, randState);
+	verifyCorrectnessOfInt_limited<64*4>(testCaseAmount, randState);
+	verifyCorrectnessOfInt_limited<64*4 + 6>(testCaseAmount, randState);
+	testCaseAmount = 500;
+	verifyCorrectnessOfInt_limited<64*10>(testCaseAmount, randState, true);
+	verifyCorrectnessOfInt_limited<64*10 + 17>(testCaseAmount, randState, true);
+	verifyCorrectnessOfInt_limited<64*16>(testCaseAmount, randState); // 1024
+	verifyCorrectnessOfInt_limited<64*16 + 31>(testCaseAmount, randState); // 1051
+	testCaseAmount = 250;
+	verifyCorrectnessOfInt_limited<64*21>(testCaseAmount, randState);
+	verifyCorrectnessOfInt_limited<64*21 + 32>(testCaseAmount, randState);
+	verifyCorrectnessOfInt_limited<64*32>(testCaseAmount, randState); // 2048
+	verifyCorrectnessOfInt_limited<64*32 + 59>(testCaseAmount, randState); // 2107
+	testCaseAmount = 150;
+	verifyCorrectnessOfInt_limited<64*64>(testCaseAmount, randState); // 4096
+	verifyCorrectnessOfInt_limited<64*64 + 63>(testCaseAmount, randState); // 4159
+
+	testCaseAmount = 50;
+	verifyCorrectnessOfInt_limited<64*256>(testCaseAmount, randState); // 16384
+	verifyCorrectnessOfInt_limited<64*512>(testCaseAmount, randState); // 32768
+	testCaseAmount = 25;
+	verifyCorrectnessOfInt_limited<64*1024>(testCaseAmount, randState); // 65536
 
 	return 0;
 }
