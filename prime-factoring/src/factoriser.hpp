00a8a571fadb3145de2a47029766f846aa8e6477
Daniel Culliver <daniel.culliver@gmail.com>
2025-07-08 17:39:37 +0200

    Add more stages to Factoriser::factorise()
    Now checks primality and checks for small factors for different bounds

--- prime-factoring/src/factoriser.hpp
+++ prime-factoring/src/factoriser.hpp
@@ -14,23 +14,53 @@ namespace Factoriser {
 	std::vector<int_limited<bit_size>> factorise(int_limited<bit_size> value) {
 		typedef int_limited<bit_size> qs_int;
 		if (debug) std::cout << "Factoriser input: " << value << std::endl;
-
-		std::vector<qs_int> factors;
-		std::vector<uint64_t> small_factors = factoriser_basic::trial_division(value);
-		for (uint64_t prime : small_factors) {
-			value /= prime;
-			factors.push_back(prime);
-		}
-		if (value == 1) return factors;
-
-		factoriser_QS<bit_size> QS(debug);
+		if (factoriser_basic::is_prime(value)) return {value};
 		
-		std::vector<qs_int> big_factors = QS.quadratic_sieve(value);
-		for (qs_int& prime : big_factors) {
-			value /= prime;
-			factors.push_back(prime);
+		std::vector<qs_int> factors;
+
+		// Stage 1: trial division up to 1000
+		{
+			std::vector<uint64_t> small_factors = factoriser_basic::trial_division(value, 1000);
+			for (uint64_t prime : small_factors) {
+				value /= prime;
+				factors.push_back(prime);
+			}
+			if (value == 1) return factors;
+			if (factoriser_basic::is_prime(value)) {
+				factors.push_back(value);
+				return factors;
+			}
 		}
 
+		// Stage 2: trial division up to 1000000
+		{
+			std::vector<uint64_t> small_factors = factoriser_basic::trial_division(value);
+			for (uint64_t prime : small_factors) {
+				value /= prime;
+				factors.push_back(prime);
+			}
+			if (value == 1) return factors;
+			if (factoriser_basic::is_prime(value)) {
+				factors.push_back(value);
+				return factors;
+			}
+		}
+
+		// Stage 3: quadratic sieve until value is a strong probable prime to 25 bases
+		{
+			factoriser_QS<bit_size> QS(debug);
+			
+			do {
+				std::vector<qs_int> big_factors = QS.quadratic_sieve(value);
+				for (qs_int& prime : big_factors) {
+					value /= prime;
+					factors.push_back(prime);
+				}
+			} while (!factoriser_basic::is_prime(value));
+		}
+		
+		// value should be either a strong probable prime or 1 at this point
+		if (value != 1) factors.push_back(value);
 		return factors;
 	};
 }
