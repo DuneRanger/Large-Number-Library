3e8216b0f3b290cb0747920d5c066b32d5477d14
Daniel Culliver <daniel.culliver@gmail.com>
2025-07-08 16:16:29 +0200

    Add upper bound to trial division

--- prime-factoring/src/factoriser_basic.hpp
+++ prime-factoring/src/factoriser_basic.hpp
@@ -10,12 +10,13 @@ namespace factoriser_basic {
 	using largeNumberLibrary::int_limited;
 
 	// Finds all factors less than or equal to 1_000_003
-	std::vector<uint64_t> trial_division(uint64_t value) {
+	std::vector<uint64_t> trial_division(uint64_t value, uint64_t upperBound = 0) {
 		std::vector<uint64_t> factors;
 		std::ifstream primes("./src/primes.txt");
 		uint64_t prime;
+		if (!upperBound) upperBound = UINT64_MAX;
 		uint64_t max = std::ceil(std::sqrt(value));
-		while (primes >> prime) {
+		while (primes >> prime && prime < upperBound) {
 			if (max < prime) {
 				factors.push_back(uint64_t(value));
 				break;
@@ -32,12 +33,13 @@ namespace factoriser_basic {
 
 	// Finds all factors less than or equal to 1_000_003
 	template<int bits>
-	std::vector<uint64_t> trial_division(int_limited<bits> value) {
+	std::vector<uint64_t> trial_division(int_limited<bits> value, uint64_t upperBound = 0) {
 		std::vector<uint64_t> factors;
 		std::ifstream primes("./src/primes.txt");
-		int prime;
+		uint64_t prime;
+		if (!upperBound) upperBound = UINT64_MAX;
 		int_limited<bits> max = value.isqrt()+1;
-		while (primes >> prime) {
+		while (primes >> prime && prime < upperBound) {
 			if (max < prime) {
 				factors.push_back(uint64_t(value));
 				break;
@@ -53,11 +55,12 @@ namespace factoriser_basic {
 	}
 
 	// Simple trial division, should work up to 10^12
-	bool is_small_prime(uint64_t value) {
+	bool is_small_prime(uint64_t value, uint64_t upperBound = 0) {
 		std::ifstream primes("./primes.txt");
 		uint64_t prime;
+		if (!upperBound) upperBound = UINT64_MAX;
 		uint64_t max = std::ceil(std::sqrt(value));
-		while (primes >> prime) {
+		while (primes >> prime && prime < upperBound) {
 			if (prime > max) return true;
 			while (!(value % prime)) value /= prime;
 			if (value == 1) return true;
@@ -68,11 +71,12 @@ namespace factoriser_basic {
 
 	// Simple trial division, should work up to 10^12
 	template<int bits>
-	bool is_small_prime(int_limited<bits> value) {
+	bool is_small_prime(int_limited<bits> value, uint64_t upperBound = 0) {
 		std::ifstream primes("./primes.txt");
 		uint64_t prime;
+		if (!upperBound) upperBound = UINT64_MAX;
 		int_limited<bits> max = value.isqrt()+1;
-		while (primes >> prime) {
+		while (primes >> prime && prime < upperBound) {
 			if (prime > max) return true;
 			while (!(value % prime)) value /= prime;
 			if (value == 1) return true;
